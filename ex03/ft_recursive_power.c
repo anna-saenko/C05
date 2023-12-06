c7ea3f296587351e0c4d6064622d79d310ab6444
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2023-12-06 15:18:54 +0100

    added ex03

--- ex03/ft_recursive_power.c
+++ ex03/ft_recursive_power.c
@@ -10,4 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_recursive_power(int nb, int power)
+{
+		if (power < 0)
+	{
+		return (0);
+	}
+	else if (power == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (nb * ft_recursive_power(nb, power - 1));
+	}
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int	result;
 
+	result = ft_recursive_power(-3, 3);
+	printf("Result: %d\n", result);
+} */
