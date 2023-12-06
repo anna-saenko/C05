0e94168018b358fb965b5f2f12503b02202a93d2
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2023-12-06 16:54:38 +0100

    add ex07

--- ex07/ft_find_next_prime.c
+++ ex07/ft_find_next_prime.c
@@ -10,4 +10,51 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_is_prime(int nb)
+{
+	int	i;
 
+	if (nb > 1)
+	{
+		i = 2;
+		while (i <= nb / 2)
+		{
+			if (nb % i == 0)
+			{
+				return (0);
+			}
+			i++;
+		}
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+int	ft_find_next_prime(int	nb)
+{
+	if (ft_is_prime(nb))
+	{
+		return (nb);
+	}
+	else
+	{
+		return (ft_find_next_prime(nb + 1));
+	}
+}
+/*
+#include <stdio.h>
+int main(void)
+{
+	int i;
+	int result;
+
+	i = -3;
+	while (i++ < 50)
+	{
+		result = ft_find_next_prime(i);
+		printf("The next prime of %d is %d\n", i, result);
+	}
+} */
