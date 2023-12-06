02c48fee195e6f4fe1a78df954972642a60cd0ae
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2023-12-06 15:59:30 +0100

    added ex05

--- ex05/ft_sqrt.c
+++ ex05/ft_sqrt.c
@@ -10,4 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_sqrt(int nb)
+{
+	int	i;
 
+	i = 0;
+	if (nb >= 0)
+	{
+		while (i * i <= nb)
+		{
+			if (i * i == nb)
+			{
+				return (i);
+			}
+			i++;
+		}
+	}
+	return (0);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int	i;
+	int	result;
+
+	i = -2;
+	while (i <= 100)
+	{
+		result = ft_sqrt(i);
+		printf("Square root of %d: %d\n", i, result);
+		i++;
+	}
+} */
