461816a1ba1e2a280a3b198febba94d34d77987f
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2023-12-06 14:42:13 +0100

    added ex00

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -10,4 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
 
+	if (!nb || nb < 0)
+	{
+		return (0);
+	}
+	else
+	{
+		if (nb == 0)
+		{
+			return (1);
+		}
+		else
+		{
+			result = nb;
+			while (--nb != 0)
+			{
+				result *= nb;
+			}
+			return (result);
+		}
+	}
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int	factorial;
+
+	factorial = ft_iterative_factorial(5);
+	printf("Factorial is: %d\n", factorial);
+}*/
